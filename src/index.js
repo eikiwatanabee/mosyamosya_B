80211189fe148be4d43af0c2f58eccc711615b80
kazuki663 <kazkaz.t@icloud.com>
2022-02-06 11:16:35 +0900

    reset.css実装

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// for( const i = 0; i < 11; i++){
+//   let member = 
+//    `<div class = "image-container">`
+//   +`<img src = "/src/img2/item${i+1}.jpg">`
+//   +`<p><p>`
+//   +`<p><p>`
+//   +`</div>`
+
+// }
